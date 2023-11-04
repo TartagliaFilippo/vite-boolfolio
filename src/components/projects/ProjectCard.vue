797699e2d410bf0d88751cfde521e25d6a692bd7
filippo199600 <pippotartaglia@gmail.com>
2023-11-04 13:18:53 +0100

    feat card

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  props: {
+    project: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h4 class="card-title">{{ project.title }}</h4>
+        <h5
+          class="card-subtitle badge"
+          :style="{ backgroundColor: project.type.color }"
+        >
+          {{ project.type.label }}
+        </h5>
+        <div>
+          <p
+            class="badge rounded-pill mt-2 mx-1"
+            v-for="technology in project.technologies"
+            :style="{ backgroundColor: technology.color }"
+          >
+            {{ technology.label }}
+          </p>
+        </div>
+      </div>
+      <div class="card-footer">
+        <a :href="project.url" class="btn btn-outline-primary">Vedi</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
