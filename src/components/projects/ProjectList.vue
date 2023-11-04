e187f020b1c44e3a4afdc3c1eec9a3fddf826968
filippo199600 <pippotartaglia@gmail.com>
2023-11-04 13:18:30 +0100

    feat list of projects

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,14 +1,22 @@
 <script>
+import ProjectCard from "./ProjectCard.vue";
+
 export default {
   data() {
     return {};
   },
+  props: { projects: Array },
+
+  components: { ProjectCard },
 };
 </script>
 
 <template>
   <div class="container">
     <h2>ProjectList</h2>
+    <div class="row row-cols-3 g-4">
+      <ProjectCard v-for="project in projects" :project="project" />
+    </div>
   </div>
 </template>
 
