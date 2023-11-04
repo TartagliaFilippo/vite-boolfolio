d2cd816d2866a3aa5c02d75e0cae593416bf47ac
filippo199600 <pippotartaglia@gmail.com>
2023-11-04 11:13:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { ProjectList },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Sono un titolo</h1>
+    <div class="section-list">
+      <ProjectList />
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h2>ProjectList</h2>
+  </div>
+</template>
+
+<style lang="scss"></style>
